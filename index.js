c370619b863724a4697f21642f68a331f5a1cc36
1980s-housewife <smlfulford@gmail.com>
2022-08-11 21:27:21 +0100

    Aborting commit due to an empty commit message.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const config = require('./config.json');
 const { Client, ActivityType } = require("discord.js-selfbot");
 const client = new Client();
-
+console.log("Loading, this may take up to 10 seconds...");
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setStatus(config.status);

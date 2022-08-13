6bfffa88fbe51500de5e28e4831f633d09c1d558
1980s-housewife <smlfulford@gmail.com>
2022-08-13 11:55:46 +0100

    actually push changes to remove status :facepalm:

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const client = new Client();
 console.log("Loading, this may take up to 10 seconds...");
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
-    if(Boolean(config.status)){
-        client.user.setStatus(config.status);
-    }
     if(Boolean(config.activity)){
         client.user.setActivity(config.activity);
     }
